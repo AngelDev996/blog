8e79eb7f8d70b817c981882ecf5f1d9745c71f34
Angel Valladares <arivalladares2.0@gmail.com>
2022-09-09 17:16:53 -0600

    update blog

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,6 +1,6 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
+  title: " ",
+  description: " ",
   lang: "en-GB",
   ogLocale: "en_GB",
   githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
